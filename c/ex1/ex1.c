1279e28775fc8e43086f6be5caf6a02d58ceb9dd
Liron Sade <liron@cs.huji.ac.il>
2017-08-06 22:49:06 +0300

    some

--- c/ex1/ex1.c
+++ c/ex1/ex1.c
@@ -2,17 +2,17 @@
 #include <math.h>
 
 #define ALPHA 0.012299
-#define BETA 1 - ALPHA
+#define BETA (1 - ALPHA)
 
 
-long double xpos;
-long double ypos;
-long double xvelo;
-long double yvelo;
-long double time;
+long double XPOS;
+long double YPOS;
+long double XVELO;
+long double YVELO;
+long double TIME;
 long steps;
-long numsteps;
-
+long show_steps;
+long step_size;
 
 /**
  * D_1
@@ -21,7 +21,7 @@ long double d1()
 {
     return powl(
             (powl((xpos + ALPHA), 2) + powl(ypos, 2)),
-            (3/2)
+            (1.5)
         );
 }
 
@@ -32,7 +32,7 @@ long double d2()
 {
     return powl(
             (powl((xpos - BETA), 2) + powl(ypos, 2)),
-            (3/2)
+            (1.5)
         );
 }
 
@@ -55,13 +55,23 @@ long double ay()
     return ypos - 2 * xvelo - BETA * (ypos) / d1() - ALPHA * (ypos) / d2();
 }
 
+void single_step()
+{
+    xpos = xpos + xvelo * step_size;
+    ypos = ypos + yvelo * step_size;
+    xvelo = ax();
+    yvelo = ay();
+}
+
+void multiple_steps() 
+{
+    for (int i = 0; i < numsteps ;i++)
+}
 
 
 int main ()
 {
+    step_size = (time/numsteps);
     return 0;
 }
 
-void inputFromFile()
-{
-}
