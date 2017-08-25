6c77427bab62a9c4288bac157b7c01bc93c15bc1
Liron Sade <liron@cs.huji.ac.il>
2017-08-25 20:56:53 +0300

    ex3

---
+++ c/ex3/GenericDFS.c
@@ -0,0 +1,10 @@
+/*
+ * implement with DFS
+ */
+pNode getBest(pNode head, getNodeChildrenFunc getChildren, \
+             getNodeValFunc getVal, freeNodeFunc freeNode, copyNodeFunc copy, unsigned int best)
+{
+    
+
+    return NULL; //best was not found.
+}
