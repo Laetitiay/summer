3e6d3d4330e5425866bf300dbd3c01f9497c55e7
Liron Sade <liron@cs.huji.ac.il>
2017-08-29 22:08:10 +0300

    hm

---
+++ cpp/ex1/Point.cpp
@@ -0,0 +1,24 @@
+class Point
+{
+public:
+    Point(long x, long y);
+
+private:
+
+    long _x;
+    long _y;
+};
+
+/*
+ * TODO:
+ * constructor
+ * deconstructor
+ * set
+ * toString
+ */
+
+// constructor of point
+Point::Point(long int x, long int y)
+{
+
+}
---
+++ cpp/ex1/PointSet.cpp
@@ -0,0 +1,22 @@
+class PointSet {
+
+public:
+
+private:
+
+/*
+ * TODO:
+ * constructor
+ * deconstructor
+ * toString
+ * add
+ * remove
+ * size
+ *
+ * operators:
+ * !=operator
+ * ==operator
+ * -operator
+ * =operator
+ */
+};
